acdf08b436a750a5526b43ac650bfe6a1af5cda2
mtom92 <torresmoranm@gmail.com>
2019-09-23 23:48:08 -0700

    adding valid anagram problem

---
+++ Week-1/ValidAnagram.js
@@ -0,0 +1,32 @@
+var isAnagram = function(s, t) {
+
+
+    if(s.length === t.length){
+        var sMap= mapping(s)
+        var tMap= mapping(t)
+
+            function mapping(word){
+            var result={}
+            for(var i=0;i<word.length;i++){
+                if(result[word[i]]){
+                    result[word[i]]++
+                   }
+                else{
+                    result[word[i]]=1
+                }
+            }
+            return result
+        }
+
+        for(var letter in sMap){
+            if(sMap[letter] != tMap[letter]){
+                  return false
+               }
+        }
+        return true
+       }
+    else{
+        return false
+    }
+
+};
