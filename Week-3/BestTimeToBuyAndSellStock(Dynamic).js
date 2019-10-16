0eac1d781c53e807db677e0104031a69cbaec401
mtom92 <torresmoranm@gmail.com>
2019-10-16 15:07:00 -0700

    adding best time to buy and sell stock dynamic  problem added

---
+++ Week-3/BestTimeToBuyAndSellStock(Dynamic).js
@@ -0,0 +1,28 @@
+var maxProfit = function(prices) {
+
+    if(prices.length <= 1) return 0;
+
+    var maxAfterIndexI = []
+
+    var max = prices[prices.length-1];
+
+    for(var i = prices.length - 2; i >= 0; i--)
+    {
+        if(prices[i] > max)
+            max = prices[i];
+        maxAfterIndexI[i] = max;
+
+    }
+
+    var maxProfit = 0;
+
+    for(var i = 0; i < prices.length; i++)
+    {
+        if(maxAfterIndexI[i] - prices[i] > maxProfit)
+            maxProfit = maxAfterIndexI[i] - prices[i];
+
+    }
+
+    return maxProfit;
+};
+ 
