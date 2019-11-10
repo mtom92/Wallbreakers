0828a387fef13a00a34ca74f3a92e40acd49f660
mtom92 <torresmoranm@gmail.com>
2019-11-09 22:37:12 -0800

    intersection of two linked list  added

---
+++ Week-4/IntersectionOfTwoLinkedLists.js
@@ -0,0 +1,20 @@
+var getIntersectionNode = function(headA, headB) {
+    var curA = headA,
+        curB = headB,
+        counter = 0
+    while(curA&&curB){
+        console.log(curA,"A")
+        console.log(curB,"B")
+        console.log(counter)
+        if(curA === curB) return curA
+        else{
+            if(!curA.next){curA = headB;counter++}
+            else curA = curA.next
+            if(!curB.next){curB = headA;counter++}
+            else curB = curB.next
+            if(counter===4)break
+        }
+        console.log("*******")
+    }
+    return null
+};
